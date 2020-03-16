b1ce0a1a5b5336b37c21b6705070e77264b31938
Max Schafer <maxsg5@hotmail.com>
2020-03-16 16:53:11 -0600

    table

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ let data = '[{"Location":"Repatriated-travellers","confirmed":"4","probable":"0"
 window.onload = function(){
   
   let file = this.JSON.parse(data);
-  let outputData = "<table><tbody> <tr> <th>Location</th> <th># of Confirmed Cases</th> <th># of Probable Cases</th></tr>";
-  for(var x = 0; x < file.length-1; x++)
+  let outputData = "<table><tbody> <tr> <th></th> <th># of Confirmed Cases</th> <th># of Probable Cases</th></tr>";
+  for(var x = 0; x < file.length; x++)
   {
     outputData += " <tr><td>"+file[x].Location.toString() +"</td><td>"+file[x].confirmed.toString() +"</td><td>"+file[x].probable.toString() +"</td></tr> ";
   }
