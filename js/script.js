8745cff299d19b3af1668e4cbb78ff3de0258839
Max Schafer <maxsg5@hotmail.com>
2020-03-16 17:17:37 -0600

    update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let data = '[{"Location":"Repatriated-travellers","confirmed":"4","probable":"0"
 window.onload = function(){
   dataOut = document.getElementById('data');
   let file = this.JSON.parse(data);
-  let outputData = "<table><tbody> <tr> <th></th> <th># of Confirmed Cases</th> <th># of Probable Cases</th></tr>";
+  let outputData = "<table class="+"minimalistBlack"+"><tbody> <tr> <th></th> <th># of Confirmed Cases</th> <th># of Probable Cases</th></tr>";
   for(var x = 0; x <= file.length-1; x++)
   {
     outputData += " <tr><td>"+file[x].Location.toString() +"</td><td>"+file[x].confirmed.toString() +"</td><td>"+file[x].probable.toString() +"</td></tr> ";
