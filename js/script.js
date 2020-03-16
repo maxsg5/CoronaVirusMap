9c270f141d7f32f6d8e745144f034a9806514185
Max Schafer <maxsg5@hotmail.com>
2020-03-16 16:11:02 -0600

    data upgrade

--- js/script.js
+++ js/script.js
@@ -1,11 +1,74 @@
 
-let data;
+let data = [
+  {
+      "Location": "Repatriated-travellers",
+      "confirmed": "4",
+      "probable": "0"
+  },
+  {
+      "Location": "Saskatchewan",
+      "confirmed": "1",
+      "probable": "5"
+  },
+  {
+      "Location": "Manitoba",
+      "confirmed": "4",
+      "probable": "3"
+  },
+  {
+      "Location": "British Columbia",
+      "confirmed": "73",
+      "probable": "0"
+  },
+  {
+      "Location": "Nova Scotia",
+      "confirmed": "0",
+      "probable": "3"
+  },
+  {
+      "Location": "New Brunswick",
+      "confirmed": "1",
+      "probable": "5"
+  },
+  {
+      "Location": "PEI",
+      "confirmed": "1",
+      "probable": "0"
+  },
+  {
+      "Location": "Alberta",
+      "confirmed": "56",
+      "probable": "0"
+  },
+  {
+      "Location": "Quebec",
+      "confirmed": "39",
+      "probable": "0"
+  },
+  {
+      "Location": "Ontario",
+      "confirmed": "145",
+      "probable": "0"
+  },
+  {
+      "Location": "Newfoundland and Labrador",
+      "confirmed": "0",
+      "probable": "1"
+  },
+  {
+      "Location": "Total Cases",
+      "confirmed": "324",
+      "probable": "14"
+  }
+];
+
 window.onload = function(){
   
-  let file = "./phac.txt";
-fetch(file).then( r => r.text() )
-.then( t => data += t )
-let txt = data.toString();
-let splitData = txt.split();    
+  let file = this.JSON.parse(data);
+//   let file = "./phac.txt";
+// fetch(file).then( r => r.text() )
+// .then( t => data += t )
+// let txt = data.toString();
+// let splitData = txt.split();    
     
 }
