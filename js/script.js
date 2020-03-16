2fd519856c84f094f1480e9f11cfbacec6d1e540
Max Schafer <maxsg5@hotmail.com>
2020-03-16 15:46:46 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ window.onload = function(){
   let file = "./phac.txt";
 fetch(file).then( r => r.text() )
 .then( t => data += t )
-
-let splitData = data.toString().split();    
+let txt = data.toString();
+let splitData = txt.split();    
     
 }
